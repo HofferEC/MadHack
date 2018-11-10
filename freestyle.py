08d44cd681a6d3c31fb904380033c8485dafda27
timschabel <43913843+timschabel@users.noreply.github.com>
2018-11-10 17:30:35 -0600

    Fixed problems when rhymes didn't exist

--- freestyle.py
+++ freestyle.py
@@ -5,7 +5,7 @@ import math
 
 #pull input word from alexa command
 
-inputWord = "man"
+inputWord = "home"
 inputHTTPS = "https://api.datamuse.com/words?ml=" + inputWord
 
 response = json.loads(requests.get(inputHTTPS).text)
@@ -20,27 +20,55 @@ relatedWords[0] = inputWord
 index = -1
 for x in range (1,len(relatedWords)):
     index += random.randint(1,3)
-    print(index)
+    #print(index)
     while " " in  response[index]['word']:
-        print (response[index]['word'])
         index += 1
     relatedWords[x] = response[index]['word']
-
-for x in range(0,len(relatedWords)):
-    print(x)
-    print (relatedWords[x])
 
 #find rhyming words with the above
 
 lastWords = []
 for word in relatedWords:
+    index = -1
     lastWords.append(word)
+
     inputHTTPS = "https://api.datamuse.com/words?rel_rhy=" + word
     response = json.loads(requests.get(inputHTTPS).text)
-    lastWords.append(response[random.randint(0,4)]['word'])
 
-for i in range (0, len(lastWords)):
-    print(lastWords[i])
+    # if the word as at least 5 rhymes, try to find a rhyme, otherwise find a word that sounds similar
+    if len(response) > 5:
+        index = random.randint(0, len(response)-1)
+
+        counter = 0
+        while " " in response[index]['word']:
+            # if we already tried to find 5 rhymes, just find a word that sounds similar
+            if counter > 5:
+                inputHTTPS = "https://api.datamuse.com/words?sl=" + word
+                response = json.loads(requests.get(inputHTTPS).text)
+                index += random.randint(1,3)
+                while " " in  response[index]['word']:
+                    index += 1
+            # word has a space in it, find a new one
+            else:
+                index = random.randint(0, len(response)-1)
+                counter += 1
+        # add whatever word was found to the list
+        lastWords.append(response[index]['word'])
+        continue
+    
+    # if there weren't enough rhymes, find a word that sounds similar
+    inputHTTPS = "https://api.datamuse.com/words?sl=" + word
+    response = json.loads(requests.get(inputHTTPS).text)
+    index += random.randint(1,3)
+    # if the word found has a space, find a new one
+    while " " in  response[index]['word']:
+        index += 1
+    lastWords.append(response[index]['word'])
+
+
+
+for i in range (0, len(lastWords), 2):
+    print(lastWords[i] + ", " + lastWords[i+1])
 
 
 #for i in range(len(relatedWords))
