3f113ff6a57c272ae2fc0e33d3276dbaa433c064
Nicholas Hoff <36050816+HofferEC@users.noreply.github.com>
2018-11-10 12:37:14 -0600

    BASELINE
    Hard coded method inputs/outputs.

---
+++ freestyle.py
@@ -0,0 +1,23 @@
+import requests
+
+#pull input word from alexa command
+
+inputWord = "car"
+
+#get related words
+
+relatedWords = ["motor", "truck", "gas", "lane", "drive"]
+
+#find rhyming words with the above
+
+rhymes = [["car, far"], ["motor, floater"], ["gas, pass"], ["lane, gain"], ["drive, jive"]]
+
+#parse SOMETHING? to develop lines ending in those given words
+
+lines = [["I'm drivin me mum's car", "Something something something far"] ,["a","a"],["b","b"],["c","c"],["d","d"]]
+
+#Put lines together into a good scheme
+
+inputWord = "car"
+
+#send output back to alexa to read out.
